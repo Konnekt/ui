f690ff83bd32fb6b60e35e9b793aa799c01f8591
Sijawusz Pur Rahnama <sija@sija.pl>
2008-06-15 14:31:34 +0000

    katalog `include`
    git-svn-id: http://www.konnekt.info/svn/ui/trunk@22 f388d6ec-b380-f94f-9e7c-e751466d6d12

---
+++ include/stdafx.h
@@ -0,0 +1,13 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <windows.h>
+#include <string>
+
+#include <Stamina/Helpers.h>
+
+using namespace Stamina;
